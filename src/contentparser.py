a82f2c01880a8412402c775bfcc1289199f85584
Will Burr <williamburr98@gmail.com>
2019-10-30 17:11:33 +0000

    Transfer link processing to the HTMLLinkParser

--- src/contentparser.py
+++ src/contentparser.py
@@ -6,21 +6,24 @@ from urlhandling import *
 class ContentParser:
 
     def parse(self, url: str, content: str) -> List[str]:
-        html_parser = HTMLLinkParser()
+        html_parser = HTMLLinkParser(origin_url=url)
         html_parser.feed(content)
-        absolute_urls, other_urls = [], []
-        for new_url in html_parser.new_urls:
-            (absolute_urls if is_http_url(new_url) else other_urls).append(new_url)
-        relative_urls = filter(lambda u: is_relative_url(u), other_urls)
-        combined_urls = list(map(lambda u: construct_absolute_url(url, u), relative_urls))
-        return list(map(normalise_url, absolute_urls + combined_urls))
+        return list(map(normalise_url, html_parser.new_urls))
 
 
 class HTMLLinkParser(html.parser.HTMLParser):
 
-    def __init__(self):
+    def __init__(self, origin_url):
         super().__init__()
         self.new_urls = []
+        self.origin_url = origin_url
+
+    def process_link(self, link):
+        if is_http_url(link):
+            self.new_urls.append(link)
+        if is_relative_url(link):
+            self.new_urls.append(construct_absolute_url(self.origin_url, link))
+
 
     def handle_starttag(self, tag, attrs):
         # Only interested in <a> tags
@@ -28,7 +31,8 @@ class HTMLLinkParser(html.parser.HTMLParser):
             return
         for attr in attrs:
             if attr[0] == 'href':
-                self.new_urls.append(attr[1])
+                self.process_link(attr[1])
+                break
 
     def error(self, message):
         """
