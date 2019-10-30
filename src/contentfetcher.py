890c432f128f1c0f53f2488acf416846e002d630
Will Burr <williamburr98@gmail.com>
2019-10-30 23:18:01 +0000

    Add handling of compression to content fetcher

--- src/contentfetcher.py
+++ src/contentfetcher.py
@@ -1,12 +1,39 @@
-from urllib import request
-from typing import Optional, List
+from urllib.request import Request, urlopen
+from http.client import HTTPResponse
 from urllib.error import HTTPError, URLError
+from typing import Optional, List
+import gzip
+import zlib
+
+
+def decompress_gzip(content: bytes) -> bytes:
+    """
+    Decompress content compressed with gzip
+    :param content: content to decompress
+    :return:
+    """
+    return gzip.decompress(content)
+
+
+def decompress_deflate(content: bytes) -> bytes:
+    """
+    Decompress content compressed with deflate
+    :param content: content to decompress
+    :return:
+    """
+    return zlib.decompress(content)
 
 
 class ContentFetcher:
     """
     Responsible for fetching content from a given URL.
     """
+
+    decompressors = {
+        "gzip": decompress_gzip,
+        "deflate": decompress_deflate
+    }
+
     def __init__(self, user_agents: List[str]):
         """
         :param user_agents: list of user agent strings to cycle through for request headers
@@ -26,20 +53,53 @@ class ContentFetcher:
         self.user_agent_index = (self.user_agent_index + 1) % len(self.user_agents)
         return user_agent
 
+    def decompress_content(self, content: bytes, encoding: str) -> bytes:
+        """
+        Attempt to decompress the content given the encoding
+        :param content: content to decompress
+        :param encoding: encoding as extracted from response header
+        :return: decompressed bytes if successful, empty byte string if not
+        """
+        if encoding in self.decompressors:
+            return self.decompressors[encoding](content)
+        return b''
+
     def retrieve_page(self, url: str) -> str:
         """
         Attempts to fetch the web page at the provided url, returning its contents as a string.
         :param url: the url to fetch from.
         :return: contents of fetched web page, or empty string if fetching failed
         """
-        url_request = request.Request(url)
+        request = self.construct_request(url)
+        try:
+            response = urlopen(request)
+            return self.handle_response(response)
+        except (HTTPError, URLError) as e:
+            print(e)
+            return ""
+
+    def construct_request(self, url: str) -> Request:
+        """
+        Build the HTTP request for the URL given
+        :param url: url to request as a string
+        :return: an urllib Request object
+        """
+        url_request = Request(url)
         user_agent = self.get_next_user_agent()
         if user_agent is not None:
             url_request.add_header("User-Agent", user_agent)
         url_request.add_header("Accept", "text/html")
-        try:
-            return request.urlopen(url_request).read().decode("utf-8", "ignore")
-        except (HTTPError, URLError, UnicodeDecodeError) as e:
-            print(e)
-            return ""
+        url_request.add_header("Accept-Encoding", "gzip, deflate")
+        return url_request
 
+    def handle_response(self, response: HTTPResponse) -> str:
+        """
+        Handle the HTTP response, including any encoding and compression
+        :param response: a HTTP response object received from a web site
+        :return: string containing the decoded, decompressed page contents
+        """
+        content = response.read()
+        encoding = response.getheader("Content-Encoding")
+        if encoding is not None:
+            content = self.decompress_content(content, encoding)
+        return content.decode("utf-8", "ignore")
